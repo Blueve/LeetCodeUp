04d52015c6dd20d72c3c52becbf4d919e2f94603
Blueve <672454911@qq.com>
2016-04-16 19:20:56 +0800

    Supported for 'tag' page

--- plugin.js
+++ plugin.js
@@ -22,14 +22,20 @@ function lockedProblemFilterAppend(){
 	}
 }
 
-$("#filterchosen").parent().after("<div class='row col-md-1'></div><div class='row col-md-4'><div class='checkbox'><label><input type='checkbox' id='b_showlocked' " + (show_locked ? "checked" : "") + "/>Show Locked Problems</label></div></div><div class='row col-md-1></div><div class='row col-md-1></div><div class='row col-md-1></div>")
+var tab_url = window.location.href;
+if(tab_url.indexOf("tag") == -1) {
+	$("#filterchosen").parent().after("<div class='row col-md-1'></div><div class='row col-md-4'><div class='checkbox'><label><input type='checkbox' id='b_showlocked' " + (show_locked ? "checked" : "") + "/>Show Locked Problems</label></div></div><div class='row col-md-1></div><div class='row col-md-1></div><div class='row col-md-1></div>")
+	$("#searchResultRow").before("<div class='row col-md-6'><form class='form-inline'><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_easy' checked />Easy</label></div><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_medium' checked />Medium</label></div><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_hard' checked />Hard</label></div><a class='btn btn-info btn-sm' id='b_pick'>Pick One</a></form></div><div class='row col-md-6'><label id='b_message'></lable></div>")
+	$("#filterchosen").change(lockedProblemFilterAppend)
+} else {
+	$("#tagCheck").before("<div class='checkbox'><label><input type='checkbox' id='b_showlocked' " + (show_locked ? "checked" : "") + "/>Show Locked Problems</label></div>")
+	$(".sortable").click(function(){
+		setTimeout("lockedProblemFilter()", 100)
+	})
+}
 
 lockedProblemFilter();
-
-$("#filterchosen").change(lockedProblemFilterAppend)
 $("#b_showlocked").change(lockedProblemFilter)
-
-$("#searchResultRow").before("<div class='row col-md-6'><form class='form-inline'><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_easy' checked />Easy</label></div><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_medium' checked />Medium</label></div><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_hard' checked />Hard</label></div><a class='btn btn-info btn-sm' id='b_pick'>Pick One</a></form></div><div class='row col-md-6'><label id='b_message'></lable></div>")
 
 $("#b_pick").click(function() {
 	var easy = $("#b_pick_easy").is(':checked')
@@ -46,8 +52,10 @@ $("#b_pick").click(function() {
 		var problems = $(select).parent()
 		var rnd = Math.floor(Math.random() * problems.length)
 		var problem = problems.slice(rnd, rnd + 1).find("td:nth-child(3)")
-		if(confirm("Pick [" + problem.text().trim() + "] ?"))
-		{
+		var problem_text = problem.text().trim()
+		if(problem_text == "") {
+			$("#b_message").text("Picked a cat :P")
+		} else if(confirm("Pick [" + problem_text + "] ?")) {
 			window.location.href=problem.find("a").attr("href")
 		}
 	}
