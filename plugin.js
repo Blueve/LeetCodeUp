5fd6e254d96911fe23c8c3c58defaced3a492415
乔 <poisonlala@hotmail.com>
2016-04-19 18:58:34 +0800

    add bootstrap3

--- plugin.js
+++ plugin.js
@@ -25,7 +25,7 @@ function lockedProblemFilterAppend(){
 var tab_url = window.location.href;
 if(tab_url.indexOf("tag") == -1) {
 	$("#filterchosen").parent().after("<div class='row col-md-1'></div><div class='row col-md-4'><div class='checkbox'><label><input type='checkbox' id='b_showlocked' " + (show_locked ? "checked" : "") + "/>Show Locked Problems</label></div></div><div class='row col-md-1></div><div class='row col-md-1></div><div class='row col-md-1></div>")
-	$("#searchResultRow").before("<div class='row col-md-6'><form class='form-inline'><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_easy' checked />Easy</label></div><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_medium' checked />Medium</label></div><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_hard' checked />Hard</label></div><a class='btn btn-info btn-sm' id='b_pick' data-toggle='modal' data-target='#pickOneModal' >Pick One</a></form></div><div class='row col-md-6'><label id='b_message'></lable></div>")
+	$("#searchResultRow").before("<div class='row col-md-6'><form class='form-inline'><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_easy' checked />Easy</label></div><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_medium' checked />Medium</label></div><div class='checkbox' style='width:24%'><label><input type='checkbox' id='b_pick_hard' checked />Hard</label></div><a class='btn btn-info btn-sm' id='b_pick' >Pick One</a></form></div><div class='row col-md-6'><label id='b_message'></lable></div>")
 	$("#filterchosen").change(lockedProblemFilterAppend)
 	$("footer").before("<div class='modal fade' id='pickOneModal'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4 class='modal-title'>Pick this one?</h4></div><div class='modal-body'><p id='pickModalContent'></p></div><div class='modal-footer'><button type='button' class='btn btn-success' id='b_yes'>Yes</button><button type='button' class='btn btn-primary' id='b_repick'>Repick</button><button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button></div></div></div></div>")
 }
@@ -50,10 +50,7 @@ function pickOne() {
     var hard = $("#b_pick_hard").is(':checked')
 
     if (!easy && !medium && !hard) {
-        //$("#b_message").text("Please choose one at least.")
-        $("#pickModalContent").text("Please choose one at least.");
-        $("#b_yes").attr('disabled', true);
-        $("#b_repick").attr('disabled', true);
+        $("#b_message").text("Please choose one at least.")
     } else {
         var select = ""
         select += easy ? "tr[class!='hide'] td:nth-child(7):contains('Easy')" : "";
@@ -64,19 +61,12 @@ function pickOne() {
         var problem = problems.slice(rnd, rnd + 1).find("td:nth-child(3)")
         var problem_text = problem.text().trim()
         if (problem_text == "") {
-            //$("#b_message").text("Picked a cat :P")
-            $("#pickModalContent").text("Picked a cat :P");
-            $("#b_yes").attr('disabled', true);
-            $("#b_repick").attr('disabled', true);
+            $("#b_message").text("Picked a cat :P")
         } else {
+            $("#pickOneModal").modal('show');
             $("#pickModalContent").html("[" + problem_text + "]");
-            $("#b_yes").attr('disabled', false);
-            $("#b_repick").attr('disabled', false);
             $("#b_yes").bind("click", function () { window.location.href = problem.find("a").attr("href"); });
-            //$("#b_ensurePick").href = problem.find("a").attr("href");
-            //$("#b_message").text("");
-            //confirm("Pick [" + problem_text + "] ?")
-            //window.location.href = problem.find("a").attr("href")
+            $("#b_message").text("");
         }
     }
 }
